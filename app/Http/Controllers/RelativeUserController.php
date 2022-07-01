917d1425cf2c2a99e3b07e1aaf4b4261af56e6a9
s-peftev <s.peftev@gmail.com>
2022-07-01 17:10:30 +0300

    added: paginate RelativeUsers

--- app/Http/Controllers/RelativeUserController.php
+++ app/Http/Controllers/RelativeUserController.php
@@ -4,13 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\RelativeUserRequest;
 use App\Models\RelativeUser;
+use Illuminate\Http\Request;
 
 class RelativeUserController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $countPerPage = (int) $request->count;
+        $totalCount = RelativeUser::all()->count();
+        $allUsers = RelativeUser::simplePaginate($countPerPage);
+
         return [
-            'items' => RelativeUser::all(),
+            'items' => $allUsers,
+            'totalCount' => $totalCount,
         ];
     }
 }
